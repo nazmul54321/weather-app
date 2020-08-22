d8391196674da389ebbde31ef173f1c74d99ba05
Muhammad Nazmul Alam <nazmul_2020s@yahoo.com>
2020-08-22 15:12:06 +0200

    Change protocol to https

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const api = {
   key: "25c77b4cb823fb2a6e14056e93eb5076",
-  base: "http://api.openweathermap.org/data/2.5/",
+  base: "https://api.openweathermap.org/data/2.5/",
 };
 
 const searchbox = document.querySelector(".search-box");
@@ -27,31 +27,50 @@ function displayResults(weather) {
   city.innerText = `${weather.name}, ${weather.sys.country}`;
 
   let now = new Date();
-  let date = document.querySelector('.location .date');
+  let date = document.querySelector(".location .date");
   date.innerText = dateBuilder(now);
 
-  let temp = document.querySelector('.current .temp');
+  let temp = document.querySelector(".current .temp");
   temp.innerHTML = `${Math.round(weather.main.temp)}<span>°c</span>`;
-  
-  let weather_el = document.querySelector('.current .weather');
+
+  let weather_el = document.querySelector(".current .weather");
   weather_el.innerText = weather.weather[0].main;
 
-  let hilow = document.querySelector('.hi-low');
-  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round
-    (weather.main.temp_max)}°c`;
-  
+  let hilow = document.querySelector(".hi-low");
+  hilow.innerText = `${Math.round(weather.main.temp_min)}°c / ${Math.round(
+    weather.main.temp_max
+  )}°c`;
 }
 
-function dateBuilder (d) {
-    let months = ["January", "February", "March", "April", "May", "June",
-     "July", "August", "September", "October", "November", "December" ];
-    let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", 
-"Friday", "Saturday"];
+function dateBuilder(d) {
+  let months = [
+    "January",
+    "February",
+    "March",
+    "April",
+    "May",
+    "June",
+    "July",
+    "August",
+    "September",
+    "October",
+    "November",
+    "December",
+  ];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-let day = days[d.getDay()];
-let date = d.getDate();
-let month = months[d.getMonth()];
-let year = d.getFullYear();
+  let day = days[d.getDay()];
+  let date = d.getDate();
+  let month = months[d.getMonth()];
+  let year = d.getFullYear();
 
-return `${day} ${date} ${month} ${year}`;
+  return `${day} ${date} ${month} ${year}`;
 }
